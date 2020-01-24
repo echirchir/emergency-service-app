954cd18d5e8d366e8684e35bd83b3e779dbce4ea
sdafrica <elisha.chirchir@softwaredynamics.co>
2020-01-24 15:20:44 -0600

    Initial commit of the hospital emergency micro-service apiproject

---
+++ src/main/java/com/robertopere/emergencyservice/EmergencyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.robertopere.emergencyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyServiceApplication.class, args);
+	}
+
+}
