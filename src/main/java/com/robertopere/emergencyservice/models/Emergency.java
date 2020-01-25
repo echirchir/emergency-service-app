11d8cd9173fa00ecc3adcc78d1e28fd8b48f32a4
sdafrica <elisha.chirchir@softwaredynamics.co>
2020-01-24 18:10:40 -0600

    Add tests for the emergencies end-points

--- src/main/java/com/robertopere/emergencyservice/models/Emergency.java
+++ src/main/java/com/robertopere/emergencyservice/models/Emergency.java
@@ -6,43 +6,82 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 
 @Entity
-@Getter
-@EqualsAndHashCode
 public class Emergency{
 
     @Id 
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotNull (message = "First name is required")
+    @NotNull (message = "First name field is required")
     private String first_name;
-
-    @NotNull (message = "Last name is required")
+    @NotNull (message = "Last name field is required")
     private String last_name;
-
-    @NotNull (message = "Patient location is required")
+    @NotNull (message = "Patient location field is required")
     private String patient_location;
-
-    @NotNull (message = "Phone number is required")
+    @NotNull (message = "Phone number field is required")
     private String phone_number;
-    
-    @NotNull (message = "Description of emergency is required")
+    @NotNull (message = "Description field is required")
     private String description;
 
     public Emergency(){
 
     }
 
-    public Emergency(@NotNull String first_name, @NotNull String last_name, @NotNull String patient_location,
-            @NotNull String phone_number, @NotNull String description) {
+    public Emergency(String first_name, String last_name, String patient_location, String phone_number, String description) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.patient_location = patient_location;
         this.phone_number = phone_number;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPatient_location() {
+        return patient_location;
+    }
+
+    public void setPatient_location(String patient_location) {
+        this.patient_location = patient_location;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 
--- src/test/java/com/robertopere/emergencyservice/EmergencyServiceApplicationTests.java
+++ src/test/java/com/robertopere/emergencyservice/EmergencyServiceApplicationTests.java
@@ -1,6 +1,17 @@
 package com.robertopere.emergencyservice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import com.robertopere.emergencyservice.models.Emergency;
+import com.robertopere.emergencyservice.repository.EmergencyRepository;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +20,77 @@ class EmergencyServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Autowired
+	private EmergencyRepository repository;
+
+	@Test
+	public void givenEmergencyExists_whenEmergencyIsFetchedById_thenValidEmergencyIsReceived() throws IOException {
+
+        // Given
+		Long id = 1L;
+		
+        // When
+		final Emergency emergency = repository.findById(id).orElse(null);
+
+        // Then
+        assertEquals(emergency.getId(), id);
+    }
+
+	@Test
+    public void givenEmergencyDoesNotExist_whenEmergencyIsRetrieved_thenEmergencyIsNull() throws IOException {
+
+        // Given
+		Long id = 4L;
+		
+        // When
+		final Emergency emergency = repository.findById(id).orElse(null);
+
+        // Then
+        assertEquals(emergency, null);
+        
+	}
+	
+	@Test
+    public void givenAtLeastOneEmergency_whenEmergenciesAreFetched_thenSizeOfEmergenciesIsGreaterThanZero() throws IOException {
+
+        // Given
+		int sizeOfEmergencies = 0;
+		
+        // When
+		final List<Emergency> emergencies = (List<Emergency>) repository.findAll();
+
+        // Then
+        assertTrue(emergencies.size() > sizeOfEmergencies);
+        
+	}
+
+    @Test
+    public void givenAddNewEmergency_whenPostRequestIsExecuted_thenTheNewSizeOfEmergenciesGreaterByOne() throws IOException {
+		
+		// Given
+		List<Emergency> currentEmergencies = (List<Emergency>)repository.findAll();
+		int currentNumberOfEmergencies = currentEmergencies.size();
+
+
+		Emergency emergency = new Emergency();
+		emergency.setFirst_name("Branson");
+		emergency.setLast_name("Gitomeh");
+		emergency.setPatient_location("Thika");
+		emergency.setPhone_number("071269734");
+		emergency.setDescription("Injury from bike accident along Thika road");
+		
+        // When
+		repository.save(emergency);
+
+		List<Emergency> updatedEmergencies = (List<Emergency>)repository.findAll();
+
+		int newNumberOfEmergencies = updatedEmergencies.size();
+
+        // Then
+        assertTrue(currentNumberOfEmergencies < newNumberOfEmergencies);
+        
+    }
+
 
 }
