8207c3a699ca41723bce7baac9fef6166ba6f656
sdafrica <elisha.chirchir@softwaredynamics.co>
2020-01-24 19:08:14 -0600

    Restored all tests

--- src/test/java/com/robertopere/emergencyservice/EmergencyServiceApplicationTests.java
+++ src/test/java/com/robertopere/emergencyservice/EmergencyServiceApplicationTests.java
@@ -1,41 +1,51 @@
 package com.robertopere.emergencyservice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import com.robertopere.emergencyservice.models.Emergency;
+import com.robertopere.emergencyservice.repository.EmergencyRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class EmergencyServiceApplicationTests {
 
-	/*@Test
+	@Test
 	void contextLoads() {
-	}*/
+	}
 
-	/*@Autowired
+	@Autowired
 	private EmergencyRepository repository;
 
 	@Test
 	public void givenEmergencyExists_whenEmergencyIsFetchedById_thenValidEmergencyIsReceived() throws IOException {
 
         // Given
-		//Long id = 1L;
+		Long id = 1L;
 		
         // When
-		////final Emergency emergency = repository.findById(id).orElse(null);
+		final Emergency emergency = repository.findById(id).orElse(null);
 
         // Then
-        //assertEquals(emergency.getId(), id);
+    	assertEquals(emergency.getId(), id);
     }
 
 	@Test
     public void givenEmergencyDoesNotExist_whenEmergencyIsRetrieved_thenEmergencyIsNull() throws IOException {
 
         // Given
-		//Long id = 4L;
+		Long id = 4L;
 		
         // When
-		//final Emergency emergency = repository.findById(id).orElse(null);
+		final Emergency emergency = repository.findById(id).orElse(null);
 
         // Then
-        //assertEquals(emergency, null);
+        assertEquals(emergency, null);
         
 	}
 	
@@ -43,13 +53,13 @@ class EmergencyServiceApplicationTests {
     public void givenAtLeastOneEmergency_whenEmergenciesAreFetched_thenSizeOfEmergenciesIsGreaterThanZero() throws IOException {
 
         // Given
-		//int sizeOfEmergencies = 0;
+		int sizeOfEmergencies = 0;
 		
         // When
-		//final List<Emergency> emergencies = (List<Emergency>) repository.findAll();
+		final List<Emergency> emergencies = (List<Emergency>) repository.findAll();
 
         // Then
-        //assertTrue(emergencies.size() > sizeOfEmergencies);
+        assertTrue(emergencies.size() > sizeOfEmergencies);
         
 	}
 
@@ -57,28 +67,28 @@ class EmergencyServiceApplicationTests {
     public void givenAddNewEmergency_whenPostRequestIsExecuted_thenTheNewSizeOfEmergenciesGreaterByOne() throws IOException {
 		
 		// Given
-		//List<Emergency> currentEmergencies = (List<Emergency>)repository.findAll();
-		//int currentNumberOfEmergencies = currentEmergencies.size();
+		List<Emergency> currentEmergencies = (List<Emergency>)repository.findAll();
+		int currentNumberOfEmergencies = currentEmergencies.size();
 
 
-		// emergency = new Emergency();
-		//emergency.setFirst_name("Branson");
-		//emergency.setLast_name("Gitomeh");
-		//emergency.setPatient_location("Thika");
-		//emergency.setPhone_number("071269734");
-		//emergency.setDescription("Injury from bike accident along Thika road");
+		Emergency emergency = new Emergency();
+		emergency.setFirst_name("Branson");
+		emergency.setLast_name("Gitomeh");
+		emergency.setPatient_location("Thika");
+		emergency.setPhone_number("071269734");
+		emergency.setDescription("Injury from bike accident along Thika road");
 		
         // When
-		//repository.save(emergency);
+		repository.save(emergency);
 
-		//List<Emergency> updatedEmergencies = (List<Emergency>)repository.findAll();
+		List<Emergency> updatedEmergencies = (List<Emergency>)repository.findAll();
 
-		//int newNumberOfEmergencies = updatedEmergencies.size();
+		int newNumberOfEmergencies = updatedEmergencies.size();
 
         // Then
-        //assertTrue(currentNumberOfEmergencies < newNumberOfEmergencies);
+        assertTrue(currentNumberOfEmergencies < newNumberOfEmergencies);
         
-    }*/
+    }
 
 
 }
